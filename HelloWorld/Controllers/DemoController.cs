3280db7abcb83d2898ee8026c86d48c9043a685e
Wehrmann Krieger, Ramon <ramon.wehrmann.krieger@centric.eu>
2020-10-29 09:38:23 +0100

    Demo of feature gate

---
+++ HelloWorld/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+using HelloWorld.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace AppConfigDemo.Controllers
+{
+    //[FeatureGate(FeatureFlag.Demo)]
+    public class DemoController : Controller
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            this._logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
